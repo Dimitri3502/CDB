fd3652c24e4093acd05ed6ce10b3142f767a2394
Dimitri3502 <dbeubry@gmail.com>
2019-04-23 14:28:10 +0200

    hashcode and equals added for models

--- src/main/java/com/excilys/training/model/Company.java
+++ src/main/java/com/excilys/training/model/Company.java
@@ -24,5 +24,30 @@ public class Company extends Model{
 		return "Company [Name=" + Name + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Company other = (Company) obj;
+		if (Name == null) {
+			if (other.Name != null)
+				return false;
+		} else if (!Name.equals(other.Name))
+			return false;
+		return true;
+	}
+
 
 }
