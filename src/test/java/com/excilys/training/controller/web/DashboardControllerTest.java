7060bb5fdb5ce83b52113ee9e85498f2dc340ec0
Dimitri3502 <dbeubry@gmail.com>
2019-05-24 11:33:50 +0200

    remove path test in server tomcat has fixed the problem

--- src/test/java/com/excilys/training/controller/web/DashboardControllerTest.java
+++ src/test/java/com/excilys/training/controller/web/DashboardControllerTest.java
@@ -1,8 +1,11 @@
 package com.excilys.training.controller.web;
 
-import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.*;
+import java.util.Arrays;
+import static com.excilys.training.controller.web.CONSTANTES.*;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,31 +23,31 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.excilys.training.configuration.WebMvcConfiguration;
+import com.excilys.training.model.Computer;
 import com.excilys.training.service.ComputerService;
 import com.excilys.training.utils.TestConfig;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {DashboardControllerTest.Config.class, TestConfig.class, WebMvcConfiguration.class })
+@ContextConfiguration(classes = { DashboardControllerTest.Config.class, TestConfig.class, WebMvcConfiguration.class })
 @WebAppConfiguration
 public class DashboardControllerTest {
 	private final static String PARAMETER_PAGE = "page";
 
-    @Configuration
-    public static class Config {
+	@Configuration
+	public static class Config {
 
-	@Bean
-	@Primary
-	public ComputerService computerService() {
-	    return Mockito.mock(ComputerService.class);
+		@Bean
+		@Primary
+		public ComputerService computerService() {
+			return Mockito.mock(ComputerService.class);
+		}
+
 	}
-
-    }
-
 
 	private WebApplicationContext wac;
 
 	private MockMvc mockMvc;
-	
+
 	private ComputerService mockComputerService;
 
 	@Before
@@ -65,7 +68,36 @@ public class DashboardControllerTest {
 
 	@Test
 	public void findAll_ShouldAddComputerEntriesToModelAndRenderDashboardView() throws Exception {
-		mockMvc.perform(get("/dashboard")).andExpect(status().isOk());
+		Computer first = new Computer.Builder()
+				.setId(1L)
+                .setName("first")
+                .build();
+ 
+		Computer second = new Computer.Builder()
+				.setId(2L)
+                .setName("second")
+                .build();
+		
+		Page page = new Page();
+ 
+        when(mockComputerService.getAll(page)).thenReturn(Arrays.asList(first, second));
+        
+        
+		mockMvc.perform(get("/dashboard")).andExpect(status().isOk())
+		.andExpect(forwardedUrl("/WEB-INF/views/dashboard.jsp"))
+		.andExpect(model().attribute(ATT_COMPUTERS, hasSize(2)))
+        .andExpect(model().attribute(ATT_COMPUTERS, hasItem(
+                allOf(
+                        hasProperty(CHAMP_ID, is(1L)),
+                        hasProperty(CHAMP_COMPUTERNAME, is("first"))
+                )
+        )))
+        .andExpect(model().attribute(ATT_COMPUTERS, hasItem(
+                allOf(
+                        hasProperty(CHAMP_ID, is(2L)),
+                        hasProperty(CHAMP_COMPUTERNAME, is("second"))
+                )
+        )));
 	}
 
 }
